dabbc96f812b749d97d42d00166f031ccba77b8c
MoisaAnastasia <81031699+MoisaAnastasia@users.noreply.github.com>
2021-03-29 23:20:47 +0300

    Add files via upload

---
+++ js_dz_4/js/script.js
@@ -0,0 +1,87 @@
+let person1 = {
+    name: 'Энрике',
+    age: 30,
+    gender: 'мужчина',
+    maritalstatus: 'женат',
+    location: 'Лондон',
+    havechild: 'Энн',
+    hobbies: 'Любит пробежки по утрам и свежевыжатый сок.'
+};
+
+let person2 = {
+    name: 'Сара',
+    age: 26,
+    gender: 'женщина',
+    maritalstatus: 'замужем',
+    location: 'Лондон',
+    havechild: 'Энн',
+    education: 'юрист',
+    hobbies: 'Любит смотреть сериалы, а иногда пишет довольно неплохие статьи в местную газету.'
+};
+
+let person3 = {
+    name: 'Энн',
+    age: 4,
+    gender: 'женщина',
+    location: 'Лондон',
+    hobbies: 'Любит рисовать мелками и играть с соседским мальчиком Томми.'
+};
+
+let person4 = {
+    name: 'Томми',
+    age: 4,
+    gender: 'мужчина',
+    location: 'Лондон',
+    hobbies: 'Любит рисовать мелками и играть с соседской девочкой Энн.'
+};
+
+let person5 = {
+    name: 'Джим',
+    age: 60,
+    gender: 'мужчина',
+    maritalstatus: 'женат',
+    location: 'Манчестер',
+    havechild: 'Сара',
+    hobbies: 'Любит путешествовать по родной Англии.'
+};
+
+let person6 = {
+    name: 'Келли',
+    age: 60,
+    gender: 'женщина',
+    maritalstatus: 'замужем',
+    location: 'Манчестер',
+    havechild: 'Сара',
+    hobbies: 'Любит путешествовать по родной Англии.'
+};
+
+let person7 = {
+    name: 'Келли',
+    age: 60,
+    gender: 'женщина',
+    maritalstatus: 'замужем',
+    location: 'Манчестер',
+    havechild: 'Сара',
+    hobbies: 'Любит путешествовать по родной Англии.'
+};
+
+let person8 = {
+    name: 'unknown',
+    age: 66,
+    gender: 'женщина',
+    maritalstatus: 'замужем',
+    location: 'Ливерпуль',
+    havechild: 'Энрике',
+    hobbies: 'Любит путешествовать по родной Англии.'
+};
+
+let person9 = {
+    name: 'Николай',
+    age: 70,
+    gender: 'мужчина',
+    maritalstatus: 'женат',
+    location: 'Ливерпуль',
+    placeofbirth: 'Сибирь',
+    education: 'начальник охраны в огромном бизнес-центре',
+    hobbies: 'После работы играет первой скрипкой в Ливерпульском оркестре.'
+};
